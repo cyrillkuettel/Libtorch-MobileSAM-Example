96a341da05fbcb1058b1525f1f5a5cab16cc1368
cyrillkuettel <cyrillkuettel@gmail.com>
2024-06-08 19:48:00 +0200

    remove superfluous comments.

--- example-app/yolo.h
+++ example-app/yolo.h
@@ -1,20 +1,17 @@
-// In your header file (.h)
 #ifndef YOLO_H
 #define YOLO_H
 
 #include <torch/script.h>  // For PyTorch's JIT compiler and model loading
-   // For PyTorch's tensor operations
 #include <cstring>         // For strlen and strcpy
 #include <iostream>        // For standard I/O operations (std::cerr, std::endl)
 #include <opencv2/core.hpp>  // For basic OpenCV structures (cv::Mat, cv::Vec3b, etc.)
 #include <opencv2/imgproc.hpp>  // For image processing functions like cv::resize
-               // For std::string
 #include <vector>
 #include <iostream>
 #include <filesystem>
 namespace fs = std::filesystem;
 
-// Function declarations
+
 float* resizeAndNormalizeImage(cv::Mat& inputImage, int w, int h);
 
 void getBestBoxes(float *outputTensorFloatArray, int32_t inputWidth, int32_t inputHeight,
